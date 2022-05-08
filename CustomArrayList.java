950c85f21bd9daa3367783304f17a5944e819a3d
Joseph Kirkish <kirkish.2@gmail.com>
2022-05-07 23:50:39 -0400

    Add files via upload

---
+++ CustomArrayList.java
@@ -0,0 +1,44 @@
+package com.coderscampus.com;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	
+	Object[] items = new Object[10];
+	int itemCounter = 0;
+
+	@Override
+	public boolean add(T item) {
+
+		if (itemCounter == items.length)
+			items = growArray();
+		items[itemCounter++] = item;
+
+		return true;
+	}
+
+	@Override
+	public int getSize() {
+		
+		return itemCounter;
+	}
+
+	@Override
+	public T get(int index) {
+		// cast the arraylist you want to return to T
+		return (T) items[index];
+	}
+
+	private Object[] growArray() {
+
+		Object[] backingArray = new Object[items.length * 2];
+		System.out.println("BackingArrayLength: " + backingArray.length);
+		int counter = 0;
+		while (counter < items.length) {
+			backingArray[counter] = items[counter];
+			counter++;
+		}
+		return backingArray;
+
+	}
+
+
+}
